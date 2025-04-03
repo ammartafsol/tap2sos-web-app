346d3850e72d2067cb2d0051e5d9e3aa18ddf182
ammartafsol <ammar.js@tafsol.com>
2025-04-03 13:43:15 +0500

    updated

--- src/component/templates/Website/AboutUs/index.jsx
+++ src/component/templates/Website/AboutUs/index.jsx
@@ -1,17 +1,28 @@
-import React from 'react'
-import classes from "./AboutUs.module.css"
-import LayoutWrapper from '@/component/atoms/LayoutWrapper'
-import TopHeader from '@/component/atoms/TopHeader'
-import { Container } from 'react-bootstrap'
+import React from "react";
+import classes from "./AboutUs.module.css";
+import LayoutWrapper from "@/component/atoms/LayoutWrapper";
+import TopHeader from "@/component/atoms/TopHeader";
+import { Container } from "react-bootstrap";
 
 const AboutUs = () => {
   return (
     <LayoutWrapper>
-        <Container>
-       <TopHeader />
-        </Container>
+      <Container>
+        <TopHeader />
+        <div className={classes.header}>
+            <div className={classes?.headerChild}>
+          <h4>Empowering Businesses Digitally</h4>
+          <p>
+            At our core, we believe in collaboration and creativity. Our mission
+            is to simplify complex processes, ensuring that our users can
+            navigate their challenges effortlessly. Join us on this journey to
+            transform the way you work!
+          </p>
+          </div>
+        </div>
+      </Container>
     </LayoutWrapper>
-  )
-}
+  );
+};
 
-export default AboutUs
+export default AboutUs;
