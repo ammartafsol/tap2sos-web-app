72da6acb24a13fe4b1705aca0456e791c52df3e4
ammartafsol <ammar.js@tafsol.com>
2025-04-04 20:17:51 +0500

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,10 @@
 export const config = {
     // apiBaseUrl: "https://r1g35c2r-3026.inc1.devtunnels.ms",
-     apiBaseUrl: "https://after5-backend-dd2-caafa38c1432.herokuapp.com", // LIVE
+     apiBaseUrl: "https://tap2sos-backend-dd-8bd70d25feb3.herokuapp.com", // LIVE
     awsBaseUrl: "https://after5-development.s3.eu-north-1.amazonaws.com",
     stripePublicKey: "",
   };
   
-  
+
+
   
