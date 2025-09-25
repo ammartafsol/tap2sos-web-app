77769b8649ca0de0c03c44a6abf49722870b4a73
ammartafsol <ammar.js@tafsol.com>
2025-09-25 11:47:06 +0500

    Enhance NoDataFound component and integrate in SecurityKey
    Refactored NoDataFound to support custom text, icon, and subtext, and added styling for improved UI. Integrated NoDataFound into SecurityKey to display a user-friendly message when no patient data is found.

--- src/component/atoms/NoDataFound/NoDataFound.jsx
+++ src/component/atoms/NoDataFound/NoDataFound.jsx
@@ -1,5 +1,22 @@
 import React from "react";
+import { ImSearch } from "react-icons/im";
+import classes from "./NoDataFound.module.css";
 
-export default function NoDataFound({ style }) {
-  return <div className={style}>No Data Found</div>;
+export default function NoDataFound({ 
+  text = "No Data Found", 
+  className,
+  icon = true,
+  iconSize = 60,
+  showSubText = false,
+  subText = "Please check back later or contact support if the issue persists."
+}) {
+  return (
+    <div className={`${classes.noDataFoundContainer} ${className || ""}`}>
+      {icon && <ImSearch size={iconSize} color={"var(--secondary-color)"} />}
+      <h3 className={classes.mainText}>{text}</h3>
+      {showSubText && (
+        <p className={classes.subText}>{subText}</p>
+      )}
+    </div>
+  );
 }
