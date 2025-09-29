876275ba2ce270ae272daf9dfdae81e7819547d1
ammartafsol <ammar.js@tafsol.com>
2025-09-29 21:00:27 +0500

    Update const.js

--- src/const.js
+++ src/const.js
@@ -5,6 +5,7 @@ export const FORM_REG_EX_REPLACER = "$1 $2";
 export const NUMBER_REG_EX = /[^0-9]+/g;
 export const DECIMAL_REG_EX = /^\d+(\.\d{0,2})?$/;
 
+
 export const URL_REG_EX = /^(http|https):\/\/[^ "]+$/;
 export const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 export const phoneRegex = /^(\+?\d{1,3}[- ]?)?\d{10}$/;
