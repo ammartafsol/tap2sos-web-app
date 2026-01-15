17ce8864ab3b24963e45b26b0b6a233431c44bd8
Hamza-Tafsol <hamza.rjs.tafsol@gmail.com>
2026-01-15 18:01:15 +0500

    ShowDocuments

--- src/component/atoms/ShowDocuments/index.jsx
+++ src/component/atoms/ShowDocuments/index.jsx
@@ -1,25 +1,32 @@
 import React from "react";
+import PropTypes from "prop-types";
 import classes from "./ShowDocuments.module.css";
 import { FaFileContract } from "react-icons/fa";
-import { MediaUrl } from "../../../resources/utils/helper";
 import { MdFileDownload } from "react-icons/md";
 import { LuLoaderCircle } from "react-icons/lu";
 
 const ShowDocuments = ({ downloadDocumens, loading, item, selectedKey }) => {
   return (
     <div className={classes.filePreview}>
-      {
-        <span className={classes.previewIcon}>
-          <FaFileContract
-            title="View File"
-            size={35}
-            color="var(--royal-navy-blue)"
-          />
-        </span>
-      }
-      <div
+      <span className={classes.previewIcon}>
+        <FaFileContract
+          title="View File"
+          size={35}
+          color="var(--royal-navy-blue)"
+        />
+      </span>
+      <button
+        type="button"
         onClick={() => downloadDocumens(item?.key)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter" || e.key === " ") {
+            e.preventDefault();
+            downloadDocumens(item?.key);
+          }
+        }}
         className={classes?.download}
+        aria-label="Download document"
+        style={{ background: "none", border: "none", padding: 0, cursor: "pointer" }}
       >
         {selectedKey === item?.key ? (
           <LuLoaderCircle
@@ -34,9 +41,23 @@ const ShowDocuments = ({ downloadDocumens, loading, item, selectedKey }) => {
             color="var(--royal-navy-blue)"
           />
         )}
-      </div>
+      </button>
     </div>
   );
+};
+
+ShowDocuments.propTypes = {
+  downloadDocumens: PropTypes.func.isRequired,
+  loading: PropTypes.bool,
+  item: PropTypes.shape({
+    key: PropTypes.string.isRequired,
+  }).isRequired,
+  selectedKey: PropTypes.string,
+};
+
+ShowDocuments.defaultProps = {
+  loading: false,
+  selectedKey: undefined,
 };
 
 export default ShowDocuments;
