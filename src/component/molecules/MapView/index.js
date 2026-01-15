b71782e3e93d95a365096721a093a07d61052045
Hamza-Tafsol <hamza.rjs.tafsol@gmail.com>
2026-01-15 19:00:57 +0500

    MapView

--- src/component/molecules/MapView/index.js
+++ src/component/molecules/MapView/index.js
@@ -1,9 +1,10 @@
+import PropTypes from "prop-types";
 import { GoogleMap, Marker } from "@react-google-maps/api";
 import classes from "./MapView.module.css";
 
 export default function MapView({ data, location, className, zoomLevel = 16 }) {
   return (
-    <div className={`${classes?.container} ${className && className}`}>
+    <div className={`${classes?.container} ${className || ""}`}>
       <GoogleMap
         zoom={zoomLevel}
         center={location}
@@ -14,13 +15,40 @@ export default function MapView({ data, location, className, zoomLevel = 16 }) {
         }}
       >
         {data ? (
-          data.map((item, index) => (
-            <Marker key={index} position={item?.coordinates} />
-          ))
+          data.map((item) => {
+            const markerKey = item?.id || item?.coordinates?.lat + "-" + item?.coordinates?.lng || `marker-${Math.random()}`;
+            return (
+              <Marker key={markerKey} position={item?.coordinates} />
+            );
+          })
         ) : (
-          <Marker key={0} position={location} />
+          <Marker key="single-marker" position={location} />
         )}
       </GoogleMap>
     </div>
   );
 }
+
+MapView.propTypes = {
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+      coordinates: PropTypes.shape({
+        lat: PropTypes.number.isRequired,
+        lng: PropTypes.number.isRequired,
+      }).isRequired,
+    })
+  ),
+  location: PropTypes.shape({
+    lat: PropTypes.number.isRequired,
+    lng: PropTypes.number.isRequired,
+  }).isRequired,
+  className: PropTypes.string,
+  zoomLevel: PropTypes.number,
+};
+
+MapView.defaultProps = {
+  data: null,
+  className: "",
+  zoomLevel: 16,
+};
