cfeb356703206559ee1d36bc2e17ae3105cc7250
Hamza-Tafsol <hamza.rjs.tafsol@gmail.com>
2026-01-15 19:11:05 +0500

    SubmitSecurityModal

--- src/component/molecules/Modal/SubmitSecurityModal/SubmitSecurityModal.jsx
+++ src/component/molecules/Modal/SubmitSecurityModal/SubmitSecurityModal.jsx
@@ -1,101 +1,4 @@
-// import React, { useState } from "react";
-// import ModalSkeleton from "../ModalSkeleton/ModalSkeleton";
-// import { Input } from "@/component/atoms/Input";
-// import Button from "@/component/atoms/Button";
-// import classes from "./SubmitSecurityModal.module.css";
-// import { useFormik } from "formik";
-// import useAxios from "@/interceptor/axiosInterceptor";
-// import * as Yup from "yup";
-// import { flattenObject } from "@/resources/utils/helper";
-// import { excludedFields } from "@/const";
-
-// const SubmitSecurityModal = ({
-//   show,
-//   setShow,
-//   slug,
-//   setData,
-//   setAttachments,
-// }) => {
-//   const { Post } = useAxios();
-//   const [loading, setLoading] = useState("");
-//   const PasswordFormik = useFormik({
-//     initialValues: {
-//       password: "",
-//     },
-//     validationSchema: Yup.object({
-//       password: Yup.string().required("Password is required"),
-//     }),
-//     onSubmit: (values) => {
-//       handleSubmit(values);
-//     },
-//   });
-
-//   const handleSubmit = async (values) => {
-//     setLoading("loading");
-//     const obj = {
-//       patientNo: slug,
-//       password: values?.password,
-//     };
-//     const { response } = await Post({
-//       route: "users/patient/login",
-//       data: obj,
-//     });
-//     console.log("response",response);
-//     if (response) {
-//       setAttachments(response?.data?.user?.attachments || {});
-//       const responseData = response?.data?.user;
-//       const filteredData = flattenObject(responseData);
-//       filteredData.phoneNumber = `+${filteredData?.callingCode} ${filteredData.phoneNumber}`;
-//       filteredData.emergencyContact = `+${filteredData?.emergencyCallingCode} ${filteredData.emergencyContact}`;
-//       delete filteredData?.callingCode;
-//       delete filteredData?.emergencyCallingCode;
-
-//       setAttachments(responseData?.attachments ? responseData?.attachments : {});
-//       setShow(false);
-//       setData(filteredData);
-//       PasswordFormik.resetForm();
-//     }
-//     setLoading("");
-//   };
-
-//   const handleClose = () => {
-//     setShow(false);
-//   };
-
-//   return (
-//     <ModalSkeleton header={"Security Key"} setShow={setShow} show={show}>
-//       <Input
-//         type={"password"}
-//         placeholder={"Enter Security Key"}
-//         setter={(e) => PasswordFormik.setFieldValue("password", e)}
-//         value={PasswordFormik.values.password}
-//         errorText={
-//           PasswordFormik.touched.password && PasswordFormik.errors.password
-//         }
-//       />
-//       <div className={classes?.btnParent}>
-//         <Button
-//           onClick={handleClose}
-//           className={classes?.cancelBtn}
-//           label={"Cancel"}
-//           variant={"secondary"}
-//         />
-//         <Button
-//           onClick={() => {
-//             PasswordFormik.handleSubmit();
-//           }}
-//           className={classes?.btn}
-//           label={loading === "loading" ? "loading..." : "Submit"}
-//           variant={"gradient"}
-//           disabled={loading === "loading"}
-//         />
-//       </div>
-//     </ModalSkeleton>
-//   );
-// };
-
-// export default SubmitSecurityModal;
-
+import PropTypes from "prop-types";
 import React, { useState } from "react";
 import ModalSkeleton from "../ModalSkeleton/ModalSkeleton";
 import { Input } from "@/component/atoms/Input";
@@ -104,9 +7,7 @@ import classes from "./SubmitSecurityModal.module.css";
 import { useFormik } from "formik";
 import useAxios from "@/interceptor/axiosInterceptor";
 import * as Yup from "yup";
-import { BaseURL, flattenObject } from "@/resources/utils/helper";
-import { excludedFields } from "@/const";
-import axios from "axios";
+import { flattenObject } from "@/resources/utils/helper";
 
 const SubmitSecurityModal = ({
   show,
@@ -117,7 +18,6 @@ const SubmitSecurityModal = ({
 }) => {
   const { Post } = useAxios();
   const [loading, setLoading] = useState("");
-  const [attachments213, setAttachments213] = useState({});
   const PasswordFormik = useFormik({
     initialValues: {
       password: "",
@@ -129,45 +29,6 @@ const SubmitSecurityModal = ({
       handleSubmit(values);
     },
   });
-
-  // const handleSubmit = async (values) => {
-  //   setLoading("loading");
-  //   const obj = {
-  //     patientNo: slug,
-  //     password: values?.password,
-  //   };
-  //   const response2 = fetch(BaseURL("users/patient/login"), {
-  //     method: "POST",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //     body: JSON.stringify(obj),
-  //   })
-  //   .then(response => response.json())
-  //   .then(data => {
-  //     console.log("data 112344354233❌❌❌❌", data?.data?.user?.attachments)
-  //     setAttachments(data?.data?.user?.attachments ? data?.data?.user?.attachments : {});
-  //   })
-  //   .catch(error => console.error("Error:", error));
-  //   console.log("response2 112344354233❌❌❌❌",response2);
-  //   return
-  //   const response = await axios.post(BaseURL("users/patient/login"), obj);
-  //   console.log("response 112344354 ❌❌❌❌",response);
-  //   if (response) {
-  //     const responseData = response?.data?.data?.user;
-  //     // console.log("responseData 112344354 ❌❌❌❌",responseData);
-  //     const filteredData = flattenObject(responseData);
-  //     filteredData.phoneNumber = `+${filteredData?.callingCode} ${filteredData.phoneNumber}`;
-  //     filteredData.emergencyContact = `+${filteredData?.emergencyCallingCode} ${filteredData.emergencyContact}`;
-  //     delete filteredData?.callingCode;
-  //     delete filteredData?.emergencyCallingCode;
-  //     setAttachments(responseData?.attachments ? responseData?.attachments : {});
-  //     setShow(false);
-  //     setData(filteredData);
-  //     PasswordFormik.resetForm();
-  //   }
-  //   setLoading("");
-  // };
 
   const handleSubmit = async (values) => {
     setLoading("loading");
@@ -184,7 +45,6 @@ const SubmitSecurityModal = ({
 
     if (response) {
       const responseData = response?.data?.user;
-      setAttachments213(responseData?.attachments);
 
       if (responseData) {
         setAttachments(responseData?.attachments || {});
@@ -238,6 +98,14 @@ const SubmitSecurityModal = ({
       </div>
     </ModalSkeleton>
   );
+};
+
+SubmitSecurityModal.propTypes = {
+  show: PropTypes.bool.isRequired,
+  setShow: PropTypes.func.isRequired,
+  slug: PropTypes.string.isRequired,
+  setData: PropTypes.func.isRequired,
+  setAttachments: PropTypes.func.isRequired,
 };
 
 export default SubmitSecurityModal;
