bbefd6b7933fc6454d7b3d31d4a5c7de0f30b182
Hamza-Tafsol <hamza.rjs.tafsol@gmail.com>
2026-01-15 17:23:48 +0500

    DocumentsView

--- src/component/atoms/DocumentsView/DocumentsView.jsx
+++ src/component/atoms/DocumentsView/DocumentsView.jsx
@@ -1,11 +1,10 @@
 import React, { useState } from "react";
+import PropTypes from "prop-types";
 import classes from "./DocumentsView.module.css";
 import { FaFileContract } from "react-icons/fa";
-import useAxios from "@/interceptor/axiosInterceptor";
 
 const DocumentsView = ({ doc }) => {
   const [loading, setLoading] = useState('');
-  const { Get } = useAxios();
 
   const handleClick = async () => {
     setLoading('load');
@@ -41,7 +40,19 @@ const DocumentsView = ({ doc }) => {
   };
 
   return (
-    <div onClick={handleClick} className={classes?.fileItem}>
+    <button
+      type="button"
+      onClick={handleClick}
+      onKeyDown={(e) => {
+        if (e.key === "Enter" || e.key === " ") {
+          e.preventDefault();
+          handleClick();
+        }
+      }}
+      className={classes?.fileItem}
+      style={{ background: "none", border: "none", padding: 0, cursor: "pointer", width: "100%" }}
+      aria-label="View document"
+    >
       <div className={classes.filePreview}>
         {loading === 'load' ? (
           <div className={classes.loadingContainer}>
@@ -57,8 +68,14 @@ const DocumentsView = ({ doc }) => {
           </span>
         )}
       </div>
-    </div>
+    </button>
   );
+};
+
+DocumentsView.propTypes = {
+  doc: PropTypes.shape({
+    key: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 export default DocumentsView;
