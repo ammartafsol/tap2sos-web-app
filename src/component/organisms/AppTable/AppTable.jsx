754462e7772081c24630c3066275520f14c1a8c9
Hamza-Tafsol <hamza.rjs.tafsol@gmail.com>
2026-01-15 19:37:06 +0500

    AppTable

--- src/component/organisms/AppTable/AppTable.jsx
+++ src/component/organisms/AppTable/AppTable.jsx
@@ -1,9 +1,11 @@
+import PropTypes from "prop-types";
 import TableSkeleton from "@/component/molecules/TableSkeleton";
 import classes from "./AppTable.module.css";
 import { RECORDS_LIMIT } from "@/const";
 import PaginationComponent from "@/component/molecules/PaginationComponent";
 import NoData from "@/component/atoms/NoData/NoData";
 import { mergeClass } from "@/resources/utils/helper";
+
 export default function AppTable({
   data = [],
   tableHeader = [],
@@ -42,19 +44,22 @@ export default function AppTable({
           <table>
             <thead>
               <tr>
-                {tableHeader?.map((item, index) => (
-                  <th
-                    key={index}
-                    style={{
-                      textAlign: "left",
-                      ...(item.style && item.style),
-                    }}
-                  >
-                    {renderTableHeader
-                      ? renderTableHeader({ item: item, index })
-                      : item?.title}
-                  </th>
-                ))}
+                {tableHeader?.map((item) => {
+                  const headerKey = item?.key || item?.title || `header-${Math.random()}`;
+                  return (
+                    <th
+                      key={headerKey}
+                      style={{
+                        textAlign: "left",
+                        ...(item.style || {}),
+                      }}
+                    >
+                      {renderTableHeader
+                        ? renderTableHeader({ item, index: tableHeader.indexOf(item) })
+                        : item?.title}
+                    </th>
+                  );
+                })}
               </tr>
             </thead>
           </table>
@@ -71,35 +76,43 @@ export default function AppTable({
               <tbody>
                 {data?.length ? (
                   data?.map((item, rowIndex) => {
+                    const rowKey = item?.id || item?.key || item?._id || `row-${rowIndex}-${Math.random()}`;
                     return (
                       <tr
-                        key={rowIndex}
+                        key={rowKey}
                         className={mergeClass("row100 body", classes.bodyRow)}
                       >
                         {tableHeader?.map(
-                          ({ key, style, title, renderValue }, colIndex) => (
-                            <td
-                              key={colIndex}
-                              className={`cell100 column${colIndex + 1}`}
-                              style={{
-                                textAlign: "left",
-                                ...style,
-                              }}
-                            >
-                              {renderItem
-                                ? renderItem({
-                                    item: item[key],
-                                    colIndex,
-                                    rowIndex,
-                                    key,
-                                    title,
-                                    renderValue,
-                                  })
-                                : renderValue
-                                ? renderValue(item[key], item)
-                                : item[key]}
-                            </td>
-                          )
+                          ({ key, style, title, renderValue }, colIndex) => {
+                            const cellKey = `${rowKey}-${key || colIndex}`;
+                            let cellContent;
+                            if (renderItem) {
+                              cellContent = renderItem({
+                                item: item[key],
+                                colIndex,
+                                rowIndex,
+                                key,
+                                title,
+                                renderValue,
+                              });
+                            } else if (renderValue) {
+                              cellContent = renderValue(item[key], item);
+                            } else {
+                              cellContent = item[key];
+                            }
+                            return (
+                              <td
+                                key={cellKey}
+                                className={`cell100 column${colIndex + 1}`}
+                                style={{
+                                  textAlign: "left",
+                                  ...style,
+                                }}
+                              >
+                                {cellContent}
+                              </td>
+                            );
+                          }
                         )}
                       </tr>
                     );
@@ -121,3 +134,30 @@ export default function AppTable({
     </>
   );
 }
+
+AppTable.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.object),
+  tableHeader: PropTypes.arrayOf(
+    PropTypes.shape({
+      key: PropTypes.string,
+      title: PropTypes.string,
+      style: PropTypes.object,
+      renderValue: PropTypes.func,
+    })
+  ),
+  loading: PropTypes.bool,
+  noDataText: PropTypes.string,
+  renderItem: PropTypes.func,
+  renderTableHeader: PropTypes.func,
+  hasPagination: PropTypes.bool,
+};
+
+AppTable.defaultProps = {
+  data: [],
+  tableHeader: [],
+  loading: false,
+  noDataText: "",
+  renderItem: null,
+  renderTableHeader: null,
+  hasPagination: false,
+};
