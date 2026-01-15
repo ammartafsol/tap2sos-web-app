57529dda76678734310fce829b2bf76613e6abd9
Hamza-Tafsol <hamza.rjs.tafsol@gmail.com>
2026-01-15 20:31:36 +0500

    Login page

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -2,11 +2,7 @@ import LoginTemplate from "@/component/templates/LoginTemplate";
 import React from "react";
 
 const Page = () => {
-  return (
-    <>
-      <LoginTemplate />
-    </>
-  );
+  return <LoginTemplate />;
 };
 
 export default Page;
