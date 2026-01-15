38370dc63905a9dbca2833ae3ef54b1e24c66197
Hamza-Tafsol <hamza.rjs.tafsol@gmail.com>
2026-01-15 20:38:05 +0500

    patient slug page

--- src/app/clinic/patient/[slug]/page.js
+++ src/app/clinic/patient/[slug]/page.js
@@ -1,9 +1,16 @@
+import PropTypes from "prop-types";
 import PatientDetailTemplate from "@/component/templates/Clinics/PatientDetailTemplate";
 import React from "react";
 
 const PatientDetail = ({ params }) => {
   const { slug } = params;
   return <PatientDetailTemplate slug={slug} />;
+};
+
+PatientDetail.propTypes = {
+  params: PropTypes.shape({
+    slug: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 export default PatientDetail;
