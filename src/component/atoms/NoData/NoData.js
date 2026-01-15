1847ff2b9644742a17c08883a0bca8e2dc01c1de
Hamza-Tafsol <hamza.rjs.tafsol@gmail.com>
2026-01-15 17:50:51 +0500

    NoData

--- src/component/atoms/NoData/NoData.js
+++ src/component/atoms/NoData/NoData.js
@@ -1,15 +1,26 @@
+import PropTypes from "prop-types";
 import { ImSearch } from "react-icons/im";
 import classes from "./noData.module.css";
 
 function NoData({ text = "No Data Found", className }) {
   return (
     <div
-      className={[classes.noDataContainer, className && className].join(" ")}
+      className={[classes.noDataContainer, className || ""].join(" ")}
     >
       <ImSearch size={60} color={"var(--secondary-color)"} />
       <p>{text}</p>
     </div>
   );
 }
+
+NoData.propTypes = {
+  text: PropTypes.string,
+  className: PropTypes.string,
+};
+
+NoData.defaultProps = {
+  text: "No Data Found",
+  className: "",
+};
 
 export default NoData;
