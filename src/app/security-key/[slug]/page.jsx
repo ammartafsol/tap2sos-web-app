eba3b06c584ab29c9ad66c8690c78b0766a3e471
Hamza-Tafsol <hamza.rjs.tafsol@gmail.com>
2026-01-15 20:43:49 +0500

    security page key

--- src/app/security-key/[slug]/page.jsx
+++ src/app/security-key/[slug]/page.jsx
@@ -1,7 +1,14 @@
-import SecurityKey from '@/component/templates/SecurityKey/SecurityKey'
-import React from 'react'
+import PropTypes from "prop-types";
+import SecurityKey from '@/component/templates/SecurityKey/SecurityKey';
+import React from 'react';
 
-export default function page({params}) {
-  const {slug} = params;
-  return <SecurityKey slug={slug} />
+export default function page({ params }) {
+  const { slug } = params;
+  return <SecurityKey slug={slug} />;
 }
+
+page.propTypes = {
+  params: PropTypes.shape({
+    slug: PropTypes.string.isRequired,
+  }).isRequired,
+};
