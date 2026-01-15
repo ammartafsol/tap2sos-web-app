368b329896a05e9a8d752e20c34cb80b54a50016
Hamza-Tafsol <hamza.rjs.tafsol@gmail.com>
2026-01-15 17:46:51 +0500

    Loader

--- src/component/atoms/Loader/index.js
+++ src/component/atoms/Loader/index.js
@@ -1,17 +1,23 @@
+import PropTypes from "prop-types";
 import classes from "./Loader.module.css";
-
 import Spinner from "react-bootstrap/Spinner";
 
 export const Loader = ({ className }) => {
   return (
-    <>
-      <div className={`${classes.loaderContainer} ${className && className}`}>
-        <div className={classes.loaderBox}>
-          <Spinner animation="grow" className={classes.loader} />
-          <Spinner animation="grow" className={classes.loader} />
-          <Spinner animation="grow" className={classes.loader} />
-        </div>
+    <div className={`${classes.loaderContainer} ${className || ""}`}>
+      <div className={classes.loaderBox}>
+        <Spinner animation="grow" className={classes.loader} />
+        <Spinner animation="grow" className={classes.loader} />
+        <Spinner animation="grow" className={classes.loader} />
       </div>
-    </>
+    </div>
   );
+};
+
+Loader.propTypes = {
+  className: PropTypes.string,
+};
+
+Loader.defaultProps = {
+  className: "",
 };
