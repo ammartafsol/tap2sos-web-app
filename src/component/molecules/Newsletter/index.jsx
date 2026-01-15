830646b769d1d04d2ca1090036e7a619a1dbf6bd
Hamza-Tafsol <hamza.rjs.tafsol@gmail.com>
2026-01-15 19:17:14 +0500

    Newsletter

--- src/component/molecules/Newsletter/index.jsx
+++ src/component/molecules/Newsletter/index.jsx
@@ -1,5 +1,4 @@
-import Button from "@/component/atoms/Button";
-import { Input } from "@/component/atoms/Input";
+import PropTypes from "prop-types";
 import React from "react";
 import classes from "./Newsletter.module.css";
 
@@ -10,23 +9,15 @@ const Newsletter = ({ data }) => {
         <h2>{data?.title}</h2>
         <p>{data?.description}</p>
       </div>
-      {/* <div className={classes.fieldOption}>
-        <div className={classes.newsField}>
-          <Input
-            mainContClassName={classes.inputClass}
-            placeholder={"Email Address"}
-            inputStyle={{
-              height: "48px",
-            }}
-          />
-          <Button label={"Submit"} variant={"gradient"} />
-        </div>
-        <p>
-          We care about your data in our <span>Privacy policy</span>
-        </p>
-      </div> */}
     </div>
   );
+};
+
+Newsletter.propTypes = {
+  data: PropTypes.shape({
+    title: PropTypes.string,
+    description: PropTypes.string,
+  }).isRequired,
 };
 
 export default Newsletter;
