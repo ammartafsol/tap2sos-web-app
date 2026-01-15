0ed656fcecd69921c56319b74d07221231d8bfa4
Hamza-Tafsol <hamza.rjs.tafsol@gmail.com>
2026-01-15 20:39:32 +0500

    patient slug edit page

--- src/app/clinic/patient/[slug]/edit/page.js
+++ src/app/clinic/patient/[slug]/edit/page.js
@@ -1,9 +1,16 @@
+import PropTypes from "prop-types";
 import AddOrEditPatientTemplate from "@/component/templates/Clinics/AddOrEditPatientTemplate";
 import React from "react";
 
 const PatientEdit = ({ params }) => {
   const { slug } = params;
   return <AddOrEditPatientTemplate slug={slug} />;
+};
+
+PatientEdit.propTypes = {
+  params: PropTypes.shape({
+    slug: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 export default PatientEdit;
