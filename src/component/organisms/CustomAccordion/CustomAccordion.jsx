de6f474a8aa82bb7cc69ab27610eea8697162d04
Hamza-Tafsol <hamza.rjs.tafsol@gmail.com>
2026-01-15 19:49:33 +0500

    CustomAccordion

--- src/component/organisms/CustomAccordion/CustomAccordion.jsx
+++ src/component/organisms/CustomAccordion/CustomAccordion.jsx
@@ -1,11 +1,11 @@
 "use client";
 
-import { Fragment, useState } from "react";
+import PropTypes from "prop-types";
+import { useState } from "react";
 import Accordion from "react-bootstrap/Accordion";
 import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
 import { CiCircleMinus, CiCirclePlus } from "react-icons/ci";
 import clsx from "clsx";
-import React from "react";
 import classes from "./CustomAccordion.module.css";
 
 export default function CustomAccordion({
@@ -20,50 +20,113 @@ export default function CustomAccordion({
 }) {
   const [isOpen, setIsOpen] = useState(0);
 
+  const handleToggle = (index) => {
+    setIsOpen((pre) => (pre === index ? -1 : index));
+  };
+
+  const handleKeyDown = (e, index) => {
+    if (e.key === "Enter" || e.key === " ") {
+      e.preventDefault();
+      handleToggle(index);
+    }
+  };
+
   const renderAccordionItems = () =>
-    data?.map((item, i) => (
-      <div
-        key={i}
-        onClick={() => setIsOpen((pre) => (pre === i ? -1 : i))}
-        className={`${classes.box} ${className} ${
-          i === isOpen && classes.activeBox
-        } ${i === isOpen && activeBoxClass} ${
-          data?.length - 1 === i ? classes.noBorder : ""
-        }`}
-      >
-        <div
-          className={`${classes.header} ${
-            isOpen === i && classes.activeHeader
-          } ${headerClass}`}
+    data?.map((item, i) => {
+      const itemKey = item?.id || item?.title || `accordion-${i}`;
+      const isItemOpen = isOpen === i;
+      const isLastItem = data?.length - 1 === i;
+      
+      let iconElement;
+      if (isCustomIcon) {
+        iconElement = isItemOpen ? (
+          <IoIosArrowUp color="#454545" size={20} />
+        ) : (
+          <IoIosArrowDown color="#454545" size={20} />
+        );
+      } else {
+        iconElement = isItemOpen ? (
+          <CiCircleMinus className={clsx(iconClass, classes.icon)} />
+        ) : (
+          <CiCirclePlus className={clsx(iconClass, classes.icon)} />
+        );
+      }
+
+      return (
+        <button
+          type="button"
+          key={itemKey}
+          onClick={() => handleToggle(i)}
+          onKeyDown={(e) => handleKeyDown(e, i)}
+          className={`${classes.box} ${className} ${
+            isItemOpen && classes.activeBox
+          } ${isItemOpen && activeBoxClass} ${
+            isLastItem ? classes.noBorder : ""
+          }`}
+          style={{
+            background: "none",
+            border: "none",
+            padding: 0,
+            width: "100%",
+            textAlign: "left",
+            cursor: "pointer",
+            display: "block"
+          }}
         >
-          <h6 className={clsx(classes.h6, titleClass)}>{item?.title}</h6>
-          {!isCustomIcon ? (
-            isOpen !== i ? (
-              <CiCirclePlus className={clsx(iconClass, classes.icon)} />
-            ) : (
-              <CiCircleMinus className={clsx(iconClass, classes.icon)} />
-            )
-          ) : isOpen !== i ? (
-            <IoIosArrowDown color="#454545" size={20} />
-          ) : (
-            <IoIosArrowUp color="#454545" size={20} />
-          )}
-        </div>
-        <Accordion.Collapse eventKey={i.toString()}>
-          <div>
-            {item?.description && (
-              <p className={classes.answer}>{item?.description}</p>
-            )}
+          <div
+            className={`${classes.header} ${
+              isItemOpen && classes.activeHeader
+            } ${headerClass}`}
+          >
+            <h6 className={clsx(classes.h6, titleClass)}>{item?.title}</h6>
+            {iconElement}
           </div>
-        </Accordion.Collapse>
-      </div>
-    ));
+          <Accordion.Collapse eventKey={i.toString()}>
+            <div>
+              {item?.description && (
+                <p className={classes.answer}>{item?.description}</p>
+              )}
+            </div>
+          </Accordion.Collapse>
+        </button>
+      );
+    });
+
+  const hasData = Array.isArray(data) && data.length > 0;
+  const activeKeyValue = isOpen >= 0 ? isOpen.toString() : null;
 
   return (
     <div className={classes.mainClass}>
-      <Accordion activeKey={isOpen >= 0 ? isOpen.toString() : null}>
-        {Array.isArray(data) && data.length > 0 && renderAccordionItems()}
+      <Accordion activeKey={activeKeyValue}>
+        {hasData && renderAccordionItems()}
       </Accordion>
     </div>
   );
 }
+
+CustomAccordion.propTypes = {
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+      title: PropTypes.string.isRequired,
+      description: PropTypes.string,
+    })
+  ).isRequired,
+  headerClass: PropTypes.string,
+  activeBoxClass: PropTypes.string,
+  className: PropTypes.string,
+  titleClass: PropTypes.string,
+  mainClass: PropTypes.string,
+  iconClass: PropTypes.string,
+  isCustomIcon: PropTypes.bool,
+};
+
+CustomAccordion.defaultProps = {
+  headerClass: "",
+  activeBoxClass: "",
+  className: "",
+  titleClass: "",
+  mainClass: "",
+  iconClass: "",
+  isCustomIcon: false,
+};
