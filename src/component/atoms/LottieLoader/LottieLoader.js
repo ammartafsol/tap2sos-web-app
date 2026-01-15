61443c8f6c454aad2b55691a0ec7cff056fe8348
Hamza-Tafsol <hamza.rjs.tafsol@gmail.com>
2026-01-15 17:48:11 +0500

    LottieLoader

--- src/component/atoms/LottieLoader/LottieLoader.js
+++ src/component/atoms/LottieLoader/LottieLoader.js
@@ -1,19 +1,27 @@
-"use client"
+"use client";
+import PropTypes from "prop-types";
 import { Player } from "@lottiefiles/react-lottie-player";
-
 import classes from "./lottieLoader.module.css";
 
 const LottieLoader = ({ className }) => {
   return (
-    <div className={`${classes?.container} ${className && className}`}>
+    <div className={`${classes?.container} ${className || ""}`}>
       <Player
         autoplay
         loop
-        src={"public\lottie\loadingSecondary.json"}
+        src="/lottie/loadingSecondary.json"
         style={{ height: "300px", width: "300px" }}
       ></Player>
     </div>
   );
+};
+
+LottieLoader.propTypes = {
+  className: PropTypes.string,
+};
+
+LottieLoader.defaultProps = {
+  className: "",
 };
 
 export default LottieLoader;
