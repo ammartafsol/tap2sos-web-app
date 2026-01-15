db99834821d99c96fd3eafdf2e66691a783d26cd
Hamza-Tafsol <hamza.rjs.tafsol@gmail.com>
2026-01-15 17:49:18 +0500

    NavigateButton

--- src/component/atoms/NavigateButton/index.jsx
+++ src/component/atoms/NavigateButton/index.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import PropTypes from "prop-types";
 import classes from "./NavigateButton.module.css";
 import { BsArrowDown } from "react-icons/bs";
 
-const NavigateButton = ({color}) => {
+const NavigateButton = ({ color }) => {
   return (
     <div className={classes.downBtn}>
       <span>
@@ -10,6 +11,14 @@ const NavigateButton = ({color}) => {
       </span>
     </div>
   );
+};
+
+NavigateButton.propTypes = {
+  color: PropTypes.string,
+};
+
+NavigateButton.defaultProps = {
+  color: undefined,
 };
 
 export default NavigateButton;
