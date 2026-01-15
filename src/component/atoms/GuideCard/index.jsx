1b4ce71a8f0a26f7a384da7a822ca0a3424925ef
Hamza-Tafsol <hamza.rjs.tafsol@gmail.com>
2026-01-15 17:32:36 +0500

    GuideCard

--- src/component/atoms/GuideCard/index.jsx
+++ src/component/atoms/GuideCard/index.jsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
 import React from "react";
+import PropTypes from "prop-types";
 import classes from "./GuideCard.module.css";
-import { MediaUrl } from "@/resources/utils/helper";
 
-const GuideCard = ({ data,removeBorder }) => {
+const GuideCard = ({ data, removeBorder }) => {
   return (
     <div className={`${classes.guidCardMain} ${removeBorder && classes?.removeBorder}`}>
       <div className={classes.cardImg}>
@@ -20,6 +20,19 @@ const GuideCard = ({ data,removeBorder }) => {
       </div>
     </div>
   );
+};
+
+GuideCard.propTypes = {
+  data: PropTypes.shape({
+    image: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+  }).isRequired,
+  removeBorder: PropTypes.bool,
+};
+
+GuideCard.defaultProps = {
+  removeBorder: false,
 };
 
 export default GuideCard;
