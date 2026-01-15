daf408d4129e6d0fe35271b4da44ca2a19363857
Hamza-Tafsol <hamza.rjs.tafsol@gmail.com>
2026-01-15 16:48:11 +0500

    TermsAndConditionTemplate

--- src/component/templates/Website/TermsAndConditionTemplate/page.jsx
+++ src/component/templates/Website/TermsAndConditionTemplate/page.jsx
@@ -1,31 +1,25 @@
 "use client";
-import React from "react";
 import classes from "./TermsAndConditionTemplate.module.css";
 import LayoutWrapper from "@/component/atoms/LayoutWrapper";
 import TopHeader from "@/component/atoms/TopHeader";
-import Quill from "@/component/atoms/Quill";
 import { Container } from "react-bootstrap";
-import { useState } from "react";
 import Parser from "html-react-parser";
 import { termsAndConditionsData } from "@/developmentContent/termsAndConditionsData";
-const TermsAndConditionTemplate = () => {
 
-  // const [data, setData] = useState(_data)
+const TermsAndConditionTemplate = () => {
   const data = termsAndConditionsData;
 
   return (
-    <>
-      <LayoutWrapper>
-        <TopHeader />
-        <Container>
-          <div className={classes.termsAndConditionsContainer}>
-            <div className={classes.quillContent}> 
+    <LayoutWrapper>
+      <TopHeader />
+      <Container>
+        <div className={classes.termsAndConditionsContainer}>
+          <div className={classes.quillContent}> 
             {data?.htmlDescription && Parser(data?.htmlDescription)}
-            </div>
           </div>
-        </Container>
-      </LayoutWrapper>
-    </>
+        </div>
+      </Container>
+    </LayoutWrapper>
   );
 };
 
