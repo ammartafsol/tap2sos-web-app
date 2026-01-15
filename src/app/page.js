25f8df8d85d977d076db6e816f0a033a6dd63171
Hamza-Tafsol <hamza.rjs.tafsol@gmail.com>
2026-01-15 20:42:20 +0500

    page

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,7 @@
 import LandingView from "@/component/templates/Website/LandingView";
 import React from "react";
-// import { getApi } from "@/interceptor/server-side-getApi";
 
 export default async function LandingPage() {
-  // const data = await getApi(`cms/page/homePage`);
-  // console.log("🚀 Landing Page Data:", data)
   return <LandingView />;
 }
 
